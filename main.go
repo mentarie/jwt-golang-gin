ac4cadce9c3353d0220de06d6cf29e1d6385573b
mentari <mentari.er@gmail.com>
2021-02-18 20:32:49 +0700

    all auth in main.go

---
+++ main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/twinj/uuid"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var router = gin.Default()
+
+type User struct {
+	ID uint64       `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+type TokenDetails struct {
+	AccessToken  string
+	RefreshToken string
+	AccessUuid   string
+	RefreshUuid  string
+	AtExpires    int64
+	RtExpires    int64
+}
+type AccessDetails struct {
+	AccessUuid string
+	UserId   uint64
+}
+type Todo struct {
+	UserID uint64 `json:"user_id"`
+	Title string `json:"title"`
+}
+
+//A sample use
+var user = User{
+	ID:          1,
+	Username: "username",
+	Password: "password",
+}
+
+func Login(context *gin.Context) {
+	var u User
+	if err := context.ShouldBindJSON(&u); err != nil {
+		context.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
+		return
+	}
+	//compare the user from the request, with the one we defined:
+	if user.Username != u.Username || user.Password != u.Password {
+		context.JSON(http.StatusUnauthorized, "Please provide valid login details")
+		return
+	}
+	token, err := CreateToken(user.ID)
+	if err != nil {
+		context.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	if err != nil {
+		context.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	tokens := map[string]string{
+		"access_token":  token.AccessToken,
+		"refresh_token": token.RefreshToken,
+	}
+	context.JSON(http.StatusOK, tokens)
+}
+
+func CreateToken(userid uint64) (*TokenDetails, error) {
+	//2
+	td := &TokenDetails{}
+	td.AtExpires = time.Now().Add(time.Minute * 15).Unix()
+	td.AccessUuid = uuid.NewV4().String()
+
+	td.RtExpires = time.Now().Add(time.Hour * 24 * 7).Unix()
+	td.RefreshUuid = uuid.NewV4().String()
+
+	//1
+	var err error
+	//Creating Access Token
+	os.Setenv("ACCESS_SECRET", "jdnfksdmfksd") //this should be in an env file
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_id"] = userid
+	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	td.AccessToken, err = at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return nil, err
+	}
+	//3
+	//Creating Refresh Token
+	//Creating Refresh Token
+	os.Setenv("REFRESH_SECRET", "mcmvmkmsdnfsdmfdsjf") //this should be in an env file
+	rtClaims := jwt.MapClaims{}
+	rtClaims["refresh_uuid"] = td.RefreshUuid
+	rtClaims["user_id"] = userid
+	rtClaims["exp"] = td.RtExpires
+	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
+	td.RefreshToken, err = rt.SignedString([]byte(os.Getenv("REFRESH_SECRET")))
+	if err != nil {
+		return nil, err
+	}
+	//nilai apa yang mau dikembalikan ke func login?
+	return td, nil
+}
+
+func ExtractToken(r *http.Request) string {
+	bearToken := r.Header.Get("Authorization")
+	//normally Authorization the_token_xxx
+	strArr := strings.Split(bearToken, " ")
+	if len(strArr) == 2 {
+		return strArr[1]
+	}
+	return ""
+}
+
+func VerifyToken(r *http.Request) (*jwt.Token, error) {
+	tokenString := ExtractToken(r)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		//Make sure that the token method conform to "SigningMethodHMAC"
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+func TokenValid(r *http.Request) error {
+	token, err := VerifyToken(r)
+	if err != nil {
+		return err
+	}
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		return err
+	}
+	return nil
+}
+
+func ExtractTokenMetadata(r *http.Request) (*AccessDetails, error) {
+	token, err := VerifyToken(r)
+	if err != nil {
+		return nil, err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		accessUuid, ok := claims["access_uuid"].(string)
+		if !ok {
+			return nil, err
+		}
+		userId, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		return &AccessDetails{
+			AccessUuid: accessUuid,
+			UserId:   userId,
+		}, nil
+	}
+	return nil, err
+}
+
+//BUAT TEST
+func CreateTodo(context *gin.Context) {
+	var td *Todo
+	if err := context.ShouldBindJSON(&td); err != nil {
+		context.JSON(http.StatusUnprocessableEntity, "invalid json")
+		return
+	}
+	//you can proceed to save the Todo to a database
+	//but we will just return it to the caller here:
+	context.JSON(http.StatusCreated, td)
+}
+
+func main() {
+	router.POST("/login", Login)
+	router.POST("/todo", CreateTodo)
+	log.Fatal(router.Run(":8080"))
+}
